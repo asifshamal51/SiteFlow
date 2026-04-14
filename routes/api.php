6949159ce2a0952897cb96ed888c4d0f2fa52156
asifshamal51 <asifshamal51@gmail.com>
2026-04-14 11:08:40 +0430

    Create user by the admin created

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
 
 
 Route::post('/login', [AuthController::class, 'login']);
@@ -14,4 +15,8 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/profile', [AuthController::class, 'profile']);
     Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::post('/users', [UserController::class, 'store']);
 });
